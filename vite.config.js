dd48116811ef654325349f0f33f290b65da2f22c
Rodolfo Castillo <brodycastillo77@gmail.com>
2024-11-08 12:35:47 -0600

    creating gitignore

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,5 @@ export default defineConfig({
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
+    base: 'https://rodolfo-castillo.github.io/drizzle-front/',
 });
--- vite.config.ts
+++ vite.config.ts
@@ -18,4 +18,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base:'https://rodolfo-castillo.github.io/drizzle-front/',
 })
